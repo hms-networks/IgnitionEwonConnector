afadf8cfec199afc2450fcdfb6f6104d89e96482
TomKimsey <thk@hms.se>
2019-06-24 15:16:15 -0400

    Convert tabs to spaces in /src files
    Made all src files consistant on indentation.

--- eWonConnector-gateway/src/main/java/org/imdc/ewon/config/EwonConfigPage.java
+++ eWonConnector-gateway/src/main/java/org/imdc/ewon/config/EwonConfigPage.java
@@ -10,26 +10,26 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.wicket.Application;
 
 public class EwonConfigPage extends RecordEditForm {
-	public static final IConfigTab CONFIG_TAB = DefaultConfigTab.builder()
-			.category(ConfigCategory.TAGS)
-			.name("ewon")
-			.i18n("ewon.config.menutitle")
-			.page(EwonConfigPage.class)
-			.terms("ewon", "data mailbox", "dm")
-			.build();
+   public static final IConfigTab CONFIG_TAB = DefaultConfigTab.builder()
+         .category(ConfigCategory.TAGS)
+         .name("ewon")
+         .i18n("ewon.config.menutitle")
+         .page(EwonConfigPage.class)
+         .terms("ewon", "data mailbox", "dm")
+         .build();
 
-	public EwonConfigPage(IConfigPage configPage) {
-		super(
-			configPage,
-			null,
-			null,
-			((IgnitionWebApp) Application.get()).getContext()
-				.getPersistenceInterface()
-				.find(EwonConnectorSettings.META, 0L)
-		);
-	}
-	@Override
-	public Pair<String, String> getMenuLocation() {
-		return CONFIG_TAB.getMenuLocation();
-	}
+   public EwonConfigPage(IConfigPage configPage) {
+      super(
+         configPage,
+         null,
+         null,
+         ((IgnitionWebApp) Application.get()).getContext()
+            .getPersistenceInterface()
+            .find(EwonConnectorSettings.META, 0L)
+      );
+   }
+   @Override
+   public Pair<String, String> getMenuLocation() {
+      return CONFIG_TAB.getMenuLocation();
+   }
 }
--- eWonConnector-gateway/src/main/java/org/imdc/ewon/data/EwonsData.java
+++ eWonConnector-gateway/src/main/java/org/imdc/ewon/data/EwonsData.java
@@ -1,18 +1,18 @@
 package org.imdc.ewon.data;
 
 public class EwonsData extends DMResult {
-	EwonData[] ewons;
-	boolean moreDataAvailable = false;
-	Long transactionId = 0L;
-	public EwonData[] getEwons() {
-		return ewons;
-	}
+   EwonData[] ewons;
+   boolean moreDataAvailable = false;
+   Long transactionId = 0L;
+   public EwonData[] getEwons() {
+      return ewons;
+   }
 
-	public boolean isMoreDataAvailable() {
-		return moreDataAvailable;
-	}
+   public boolean isMoreDataAvailable() {
+      return moreDataAvailable;
+   }
 
-	public Long getTransactionId(){
-		return transactionId;
-	}
+   public Long getTransactionId(){
+      return transactionId;
+   }
 }
