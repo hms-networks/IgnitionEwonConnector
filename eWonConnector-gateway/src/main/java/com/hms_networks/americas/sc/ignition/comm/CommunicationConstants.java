4d9ef6ad977707820db5c618724a10b0e48c7afd
Alex J Hawk (HMS) <51409174+alexjhawk@users.noreply.github.com>
2023-06-02 15:38:48 -0400

    Add CommunicationConstants.java @version javadocs

--- eWonConnector-gateway/src/main/java/com/hms_networks/americas/sc/ignition/comm/CommunicationConstants.java
+++ eWonConnector-gateway/src/main/java/com/hms_networks/americas/sc/ignition/comm/CommunicationConstants.java
@@ -6,5 +6,6 @@ package com.hms_networks.americas.sc.ignition.comm;
  *
  * @author HMS Networks, MU Americas Solution Center
  * @since 2.0.0
+ * @version 1.0.0
  */
 public class CommunicationConstants {}
