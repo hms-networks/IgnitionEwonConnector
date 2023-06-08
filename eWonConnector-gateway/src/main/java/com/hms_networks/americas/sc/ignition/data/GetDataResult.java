4614410b689fd88e42bd2455da18a2411ce8d602
Alex J Hawk (HMS) <51409174+alexjhawk@users.noreply.github.com>
2023-06-08 10:48:44 -0400

    Remove outdated data pkg classes

--- eWonConnector-gateway/src/main/java/com/hms_networks/americas/sc/ignition/data/GetDataResult.java
+++
@@ -1,7 +0,0 @@
-package com.hms_networks.americas.sc.ignition.data;
-
-/** Class representation of get data request result */
-public class GetDataResult extends DMResult {
-  /** Boolean if more data is available */
-  private boolean moreDataAvailable;
-}
