1d3229e7d15f03b7ec3396079d754bcb6daedfcd
Alex J Hawk (HMS) <51409174+alexjhawk@users.noreply.github.com>
2023-07-06 20:50:02 -0400

    Update M2WebEwonUpdateTagValuesRequest
    Switch from an array of key value pairs to a
    collection, which better aligns with the
    input requirements of the connector (and most
    likely future use cases too).
    
    Additionally, switched the tag value type to
    Object from String, which allows much more
    flexibility for implementing applications.

--- eWonConnector-gateway/src/main/java/com/hms_networks/americas/sc/ignition/comm/requests/m2w/M2WebEwonUpdateTagValuesRequest.java
+++ eWonConnector-gateway/src/main/java/com/hms_networks/americas/sc/ignition/comm/requests/m2w/M2WebEwonUpdateTagValuesRequest.java
@@ -1,6 +1,7 @@
 package com.hms_networks.americas.sc.ignition.comm.requests.m2w;
 
 import com.hms_networks.americas.sc.ignition.comm.CommunicationAuthInfo;
+import java.util.Collection;
 import org.apache.commons.collections4.KeyValue;
 
 /**
@@ -28,7 +29,7 @@ public class M2WebEwonUpdateTagValuesRequest extends M2WebEwonGetRequest {
    * @param communicationAuthInfo The Talk2M account {@link CommunicationAuthInfo} object to use for
    *     the request.
    * @param ewonName The name of the Ewon to perform the update tag values request on.
-   * @param tagValuePairs The tag name and (updated) value pairs.
+   * @param tagNameValuePairs The tag name and (updated) value pairs.
    * @since 1.0.0
    */
   public M2WebEwonUpdateTagValuesRequest(
@@ -36,7 +37,7 @@ public class M2WebEwonUpdateTagValuesRequest extends M2WebEwonGetRequest {
       String t2mSessionId,
       CommunicationAuthInfo communicationAuthInfo,
       String ewonName,
-      KeyValue<String, String>[] tagValuePairs) {
+      Collection<KeyValue<String, Object>> tagNameValuePairs) {
     this(
         server,
         t2mSessionId,
@@ -44,7 +45,7 @@ public class M2WebEwonUpdateTagValuesRequest extends M2WebEwonGetRequest {
         communicationAuthInfo.getEwonUsername(),
         communicationAuthInfo.getEwonPassword(),
         ewonName,
-        tagValuePairs);
+        tagNameValuePairs);
   }
 
   /**
@@ -57,7 +58,7 @@ public class M2WebEwonUpdateTagValuesRequest extends M2WebEwonGetRequest {
    * @param t2mDeviceUsername The Talk2M device username to use for the request. Can not be null.
    * @param t2mDevicePassword The Talk2M device password to use for the request. Can not be null.
    * @param ewonName The name of the Ewon to perform the update tag values request on.
-   * @param tagValuePairs The tag name and (updated) value pairs.
+   * @param tagNameValuePairs The tag name and (updated) value pairs.
    * @since 1.0.0
    */
   public M2WebEwonUpdateTagValuesRequest(
@@ -67,7 +68,7 @@ public class M2WebEwonUpdateTagValuesRequest extends M2WebEwonGetRequest {
       String t2mDeviceUsername,
       String t2mDevicePassword,
       String ewonName,
-      KeyValue<String, String>[] tagValuePairs) {
+      Collection<KeyValue<String, Object>> tagNameValuePairs) {
     super(
         server,
         t2mSessionId,
@@ -75,37 +76,36 @@ public class M2WebEwonUpdateTagValuesRequest extends M2WebEwonGetRequest {
         t2mDeviceUsername,
         t2mDevicePassword,
         ewonName,
-        getUpdateTagValuesRequestPath(tagValuePairs));
+        getUpdateTagValuesRequestPath(tagNameValuePairs));
   }
 
   /**
    * Gets the Ewon update tag values request path for the specified tag name and (updated) value
    * pairs.
    *
-   * @param tagValuePairs The tag name and (updated) value pairs.
+   * @param tagNameValuePairs The tag name and (updated) value pairs.
    * @return The Ewon update tag values request path.
    * @since 1.0.0
    */
-  private static String getUpdateTagValuesRequestPath(KeyValue<String, String>[] tagValuePairs) {
+  private static String getUpdateTagValuesRequestPath(
+      Collection<KeyValue<String, Object>> tagNameValuePairs) {
     StringBuilder requestPath = new StringBuilder(UPDATE_TAG_VALUES_GET_REQUEST_PATH_BASE);
 
     // Add tag name and value pairs to request path
-    final int firstTagValuePairIndex = 0;
-    for (int tagValuePairIndex = 0; tagValuePairIndex < tagValuePairs.length; tagValuePairIndex++) {
+    boolean isFirstTagValuePair = true;
+    int tagValuePairIndex = 1;
+    for (KeyValue<String, Object> tagNameValuePair : tagNameValuePairs) {
       // Append query string separator (? if first parameter)
-      if (tagValuePairIndex == firstTagValuePairIndex) {
+      if (isFirstTagValuePair) {
         requestPath.append("?");
+        isFirstTagValuePair = false;
       } else {
         requestPath.append("&");
       }
 
       // Append tag name
-      final int tagIndex = tagValuePairIndex + 1;
-      requestPath
-          .append("TagName")
-          .append(tagIndex)
-          .append("=")
-          .append(tagValuePairs[tagValuePairIndex].getKey());
+      final int tagIndex = tagValuePairIndex++;
+      requestPath.append("TagName").append(tagIndex).append("=").append(tagNameValuePair.getKey());
 
       // Append query string separator
       requestPath.append("&");
@@ -115,7 +115,7 @@ public class M2WebEwonUpdateTagValuesRequest extends M2WebEwonGetRequest {
           .append("TagValue")
           .append(tagIndex)
           .append("=")
-          .append(tagValuePairs[tagValuePairIndex].getValue());
+          .append(tagNameValuePair.getValue().toString());
     }
 
     return requestPath.toString();
